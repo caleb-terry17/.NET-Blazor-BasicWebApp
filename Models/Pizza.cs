3872f959aa200badf3286ee80117616593f49774
caleb-terry17 <k.cterry30@gmail.com>
2022-12-21 16:24:33 -0600

    added dialog for getting pizzas

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -9,5 +9,10 @@
         public bool Vegetarian { get; set; }
         public bool Vegan { get; set; }
         public string ImageUrl { get; set; }
+        public int Size { get; set; }
+
+        public static int DefaultSize { get; } = 10;
+        public static int MinimumSize { get; } = 4;
+        public static int MaximumSize { get; } = 20;
     }
 }
